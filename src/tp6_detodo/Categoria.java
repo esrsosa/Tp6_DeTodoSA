5c8ca50ba6ed28353e1161c8508aa20b09554d13
esrsosa <131368173+esrsosa@users.noreply.github.com>
2023-08-30 00:46:31 -0300

    a

---
+++ src/tp6_detodo/Categoria.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp6_detodo;
+
+/**
+ *
+ * @author Emanuel Sosa
+ */
+public enum Categoria {
+        COMESTIBLE,
+        LIMPIEZA,
+        PERFUMERIA;
+}
